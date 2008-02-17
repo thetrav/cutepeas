2ccbfe72759a7be7c4cefd1e2c9f92b1ea96b7da
hartror@telstra.com <hartror@telstra.com@520e8878-2746-0410-8e0a-abc221dbbbb9>
2008-02-17 06:32:06 +0000

    Initial Add

---
+++ CutePeas/src/Tests/TestPathfinding.py
@@ -0,0 +1,40 @@
+import pygame, sys
+
+from pygame.locals import *
+from Images import *
+from UserInterface.Button import Button
+from UserInterface.UserInterface import UserInterface
+
+import PathFinding.TestBed
+
+class Game:
+    def __init__(self):
+        self.window = pygame.display.set_mode((800,600))
+        pygame.display.set_caption('Cute Peas - UI Prototype')
+        
+        self.screen = pygame.display.get_surface()
+        
+        loadImages()
+    
+    def handleInput(self, events):
+        for event in events:
+            if event.type == QUIT:
+                print 'Goodbye!'
+                sys.exit(0)
+            else :
+                self.userInterface.handleEvent(event)
+    
+    def render(self, screen):
+        screen.blit(images["Background"], (0,0))
+        PathFinding.TestBed.drawNodes(self.screen)
+        self.userInterface.render(screen)
+        pygame.display.flip()
+        
+    def main(self):
+        self.userInterface = UserInterface()
+        PathFinding.TestBed.createNodes()
+        while True:
+            self.handleInput(pygame.event.get())
+            self.render(self.screen)
+        
+Game().main()
