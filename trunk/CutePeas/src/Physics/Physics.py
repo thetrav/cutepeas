018cb842b5864104584f68aa4cb1ae6a748b1c92
bungholeyo <bungholeyo@520e8878-2746-0410-8e0a-abc221dbbbb9>
2008-02-17 04:48:38 +0000

    Updates

--- trunk/CutePeas/src/Physics/Physics.py
+++ trunk/CutePeas/src/Physics/Physics.py
@@ -1,4 +1,110 @@
+import Vector
+import Surface
+import BlockRef
+import math
+
 class SurfaceManager:
     def __init__(self):
         self.surfaces = []
+        self.nextSurfaceID = 0
+        
+        self.blocks = []
+        self.nextBlockID = 0
+        
+    def render(self, screen):
+        for surface in surfaces
+            surface.render(screen)
+        
+    
+    def addBlock(self, position, type ):
+        
+        if BlockType[type] == BlockType['normal'] :
+            
+
+    def createTopSurface(self, position):
+        xPos =  position.x;
+        yPos =  position.y + (BlockHeight / 2)
+        
+        newSurface = Surface(self.nextSurfaceID,
+                             Vector(xPos, yPos),
+                             0,
+                             BlockType['normal']
+                             BlockWidth
+                             )        
+        return newSurface
+    
+    def createBottomSurface(self, position, blockType):
+        xPos =  position.x;
+        yPos =  position.y - (BlockHeight / 2)
+        
+        newSurface = Surface(self.nextSurfaceID,
+                             Vector(xPos, yPos),
+                             0,
+                             blockType
+                             BlockWidth
+                             )        
+        return newSurface
+        
+    def createLeftSurface(self, position):
+        xPos =  position.x - (BlockWidth / 2) 
+        yPos =  position.y;
+        
+        newSurface = Surface(self.nextSurfaceID,
+                             Vector(xPos, yPos),
+                             0,
+                             blockType
+                             BlockWidth
+                             )        
+        return newSurface
+            
+    def createRightSurface(self, position):
+        xPos =  position.x + (BlockWidth / 2) 
+        yPos =  position.y;
+        
+        newSurface = Surface(self.nextSurfaceID,
+                             Vector(xPos, yPos),
+                             0,
+                             blockType
+                             BlockWidth
+                             )        
+        return newSurface
+        
+        
+    def createLeftRamp(self, position):
+        xPos =  position.x; 
+        yPos =  position.y;
+        
+        tlX = position.x - (BlockWidth/2)
+        brX = position.x + (BlockWidth/2)
+        tlY = position.y - (BlockHeight/2)
+        brY = position.y + (BlockHeight/2)
+        
+        length = math.sqrt( math.pow( (brX - tlX), 2), math.pow( (brY - tlY), 2) )
+        
+        newSurface = Surface(self.nextSurfaceID,
+                             Vector(xPos, yPos),
+                             0,
+                             blockType
+                             length
+                             )        
+        return newSurface
+        
+    def createRightRamp(self, position):
+        xPos =  position.x + (BlockWidth / 2) 
+        yPos =  position.y;
+        
+        
+        newSurface = Surface(self.nextSurfaceID,
+                             Vector(xPos, yPos),
+                             0,
+                             blockType
+                             BlockWidth
+                             )        
+        return newSurface
+        
+    
+        
+            
+        
+        
         
---
+++ trunk/CutePeas/src/Physics/__init__.py
@@ -0,0 +1,4 @@
+BlockType = {'normal': 0, 'spring': 1, 'gel ': 3 }
+BlockHeight = 61
+BlockWidth = 71
+
