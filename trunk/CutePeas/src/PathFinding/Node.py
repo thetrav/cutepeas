9a7ce4edb9f7a51cabea780cd4bfe09b1328afec
hartror@telstra.com <hartror@telstra.com@520e8878-2746-0410-8e0a-abc221dbbbb9>
2008-02-17 04:48:26 +0000

    Initial Add

---
+++ trunk/CutePeas/src/PathFinding/Node.py
@@ -0,0 +1,24 @@
+from Physics import * 
+
+def nodeLink(nodeA, nodeB):
+    nodeA.addLink(nodeB)
+    nodeB.addLink(nodeA)
+    
+class Node:
+    "Pathfinding node"
+    def __init__(self, x, y, surface):
+        self.position = Vector(x=x, y=y)
+        self.links = []
+        self.surface = surface
+        
+    def addLink(self, node):
+        if node in self.links:
+            return
+        else:
+            self.links.append(node)
+        
+    def delLink(self, node):
+        if node in self.links:
+            self.links.remove(node)
+    
+    
