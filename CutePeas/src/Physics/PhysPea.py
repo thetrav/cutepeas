8168fb2888b0725aa5a2fc6634f0b14399c94496
bungholeyo <bungholeyo@520e8878-2746-0410-8e0a-abc221dbbbb9>
2008-02-25 14:53:52 +0000

    Added Various Properties necessary for the Physical Simulation of Peas.

--- CutePeas/src/Physics/PhysPea.py
+++ CutePeas/src/Physics/PhysPea.py
@@ -1,5 +1,15 @@
-
+from Vector import Vector
 class PhysPea:
     
-    def __init__(self):
-        print('Creating a Pea')
+    def __init__(self, startVec):
+        print('Creating Pea Physics Properties')
+        self.startCoord = startVec
+        self.xvelocity = 0
+        self.yvelocity = 0
+        self.hitCoord = Vector()
+        self.radius = 17
+        self.currentCoord = startVec
+        
+        self.numBounces = 0
+        
+        self.hitSurfaces = {}
