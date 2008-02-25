c863060906f27d2e873087cdaf7143e8f3d6657d
bungholeyo <bungholeyo@520e8878-2746-0410-8e0a-abc221dbbbb9>
2008-02-25 15:05:57 +0000

    Added Pea Physics State

--- CutePeas/src/Physics/PhysPea.py
+++ CutePeas/src/Physics/PhysPea.py
@@ -3,6 +3,7 @@ class PhysPea:
     
     def __init__(self, startVec):
         print('Creating Pea Physics Properties')
+        self.state = 'walking'
         self.startCoord = startVec
         self.xvelocity = 0
         self.yvelocity = 0
